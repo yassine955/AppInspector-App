5b7414ad65eb33b4e0cdedc2c8d7d69aadbb86be
Yassine <54895428+yassine955@users.noreply.github.com>
2025-03-03 15:25:04 +0100

    Update Loading.tsx

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,7 +2,7 @@ export const LoadingComp = () => {
   return (
     <svg
       aria-hidden="true"
-      className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+      className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-darkBlueText"
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
