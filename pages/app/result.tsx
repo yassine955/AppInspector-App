aab5e487f3a1cef34ba8ca73e05792759e8f2615
Yassine <54895428+yassine955@users.noreply.github.com>
2025-03-03 15:25:16 +0100

    Update result.tsx

--- pages/app/result.tsx
+++ pages/app/result.tsx
@@ -5,8 +5,58 @@ import { useRouter } from "next/router";
 import { Fragment, useEffect, useState } from "react";
 import { VulnerableApp } from "./results";
 
+const RiskComponent = ({
+  description,
+  title,
+  riskValue,
+}: {
+  title: string;
+  description: string;
+  riskValue: number;
+}) => {
+  let happyOpacity = 0;
+  let middleOpacity = 0;
+  let angryOpacity = 0;
+  if (riskValue < 16) {
+    happyOpacity = 100;
+    middleOpacity = 20;
+    angryOpacity = 5;
+  } else if (riskValue >= 16 && riskValue <= 35) {
+    happyOpacity = 20;
+    middleOpacity = 100;
+    angryOpacity = 20;
+  } else if (riskValue > 35 && riskValue <= 50) {
+    happyOpacity = 5;
+    middleOpacity = 20;
+    angryOpacity = 100;
+  }
+
+  return (
+    <div className="flex mb-5" style={{ alignItems: "center" }}>
+      <article className="pl-4 font-bold text-blue-950 w-2/3">
+        <h1 className="text-xl mb-2">{title}</h1>
+        <p className="text-sm font-normal">{description}</p>
+      </article>
+      <div className="flex w-1/3">
+        <img
+          className="mr-2"
+          style={{ opacity: `${happyOpacity}%` }}
+          src="/happy.svg"
+        />
+        <img
+          className="mr-2"
+          style={{ opacity: `${middleOpacity}%` }}
+          src="/middle.svg"
+        />
+        <img style={{ opacity: `${angryOpacity}%` }} src="/angry.svg" />
+      </div>
+    </div>
+  );
+};
+
 export default function SingleResult() {
   const [dataRow, setDataRow] = useState<VulnerableApp>();
+
   const {
     push,
     query: { id },
@@ -55,64 +105,26 @@ export default function SingleResult() {
               </article>
             </div>
 
-            <div className="flex mb-5" style={{ alignItems: "center" }}>
-              <article className="pl-4 font-bold text-blue-950 w-2/3">
-                <h1 className="text-xl mb-2">Privacy</h1>
-                <p className="text-sm font-normal">
-                  Een app die bijvoorbeeld je email deelt - krijgt een hogere
-                  score voor privacy.
-                </p>
-              </article>
-              <div className="flex w-1/3">
-                <img className="mr-2" src="/happy.svg" />
-                <img className="mr-2" src="/middle.svg" />
-                <img src="/angry.svg" />
-              </div>
-            </div>
-            <div className="flex mb-5" style={{ alignItems: "center" }}>
-              <article className="pl-4 font-bold text-blue-950 w-2/3">
-                <h1 className="text-xl mb-2">Financiën</h1>
-                <p className="text-sm font-normal">
-                  Een app die bankgegevens niet goed beveiligt - krijgt een
-                  hoge(re) score voor Financiën.
-                </p>
-              </article>
-              <div className="flex w-1/3">
-                <img className="mr-2" src="/happy.svg" />
-                <img className="mr-2" src="/middle.svg" />
-                <img src="/angry.svg" />
-              </div>
-            </div>
-
-            <div className="flex mb-5" style={{ alignItems: "center" }}>
-              <article className="pl-4 font-bold text-blue-950 w-2/3">
-                <h1 className="text-xl mb-2">Systeem</h1>
-                <p className="text-sm font-normal">
-                  Een app die het systeem kan laten crashen - krijgt een
-                  hoge(re) score voor Systeem.
-                </p>
-              </article>
-              <div className="flex w-1/3">
-                <img className="mr-2" src="/happy.svg" />
-                <img className="mr-2" src="/middle.svg" />
-                <img src="/angry.svg" />
-              </div>
-            </div>
-
-            <div className="flex mb-5" style={{ alignItems: "center" }}>
-              <article className="pl-4 font-bold text-blue-950 w-2/3">
-                <h1 className="text-xl mb-2">Betrouwbaarheid</h1>
-                <p className="text-sm font-normal">
-                  Een app die het systeem kan laten crashen - krijgt een
-                  hoge(re) score voor Betrouwbaarheid.
-                </p>
-              </article>
-              <div className="flex w-1/3">
-                <img className="mr-2" src="/happy.svg" />
-                <img className="mr-2" src="/middle.svg" />
-                <img src="/angry.svg" />
-              </div>
-            </div>
+            <RiskComponent
+              riskValue={dataRow?.privacy_risk!}
+              title="Privacy"
+              description="Een app die bijvoorbeeld je email deelt - krijgt een hogere score voor privacy."
+            />
+            <RiskComponent
+              riskValue={dataRow?.system_risk!}
+              title="Systeem"
+              description="Een app die het systeem kan laten crashen - krijgt een hoge(re) score voor Systeem."
+            />
+            <RiskComponent
+              riskValue={dataRow?.reliability_risk!}
+              title="Betrouwbaarheid"
+              description="Een app die het systeem kan laten crashen - krijgt een hoge(re) score voor Betrouwbaarheid."
+            />
+            <RiskComponent
+              riskValue={dataRow?.financial_risk!}
+              title="Financiën"
+              description="Een app die bankgegevens niet goed beveiligt - krijgt een hoge(re) score voor Financiën."
+            />
           </div>
         )}
       </BodyComp>
