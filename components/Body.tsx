90e2163e0bc0ae12a7f8c9ac38884a55fb24cb36
Yassine <54895428+yassine955@users.noreply.github.com>
2025-03-03 22:36:18 +0100

    Update Body.tsx

--- components/Body.tsx
+++ components/Body.tsx
@@ -50,30 +50,6 @@ export const BodyComp = ({
             </span>
           </p>
         </div>
-        <div className="flex items-center">
-          <button
-            data-dismiss-target="#bottom-banner"
-            type="button"
-            className="shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white"
-          >
-            <svg
-              className="w-3 h-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-              />
-            </svg>
-            <span className="sr-only">Close banner</span>
-          </button>
-        </div>
       </div>
     </div>
   );
