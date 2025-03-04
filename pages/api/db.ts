fe5c32a2926cea409d699a7cb849a56b04fa4f6f
Yassine <54895428+yassine955@users.noreply.github.com>
2025-03-04 17:49:57 +0100

    Update db.ts

--- pages/api/db.ts
+++ pages/api/db.ts
@@ -1,12 +1,21 @@
 import { Pool } from "pg";
 
 const client = new Pool({
-  user: "neondb_owner",
-  host: process.env.NEXT_PUBLIC_HOST,
-  database: process.env.NEXT_PUBLIC_DB,
-  password: process.env.NEXT_PUBLIC_PASS,
+  user: process.env.NODE_ENV === "development" ? "postgres" : "neondb_owner",
+  host:
+    process.env.NODE_ENV === "development"
+      ? "localhost"
+      : process.env.NEXT_PUBLIC_HOST,
+  database:
+    process.env.NODE_ENV === "development"
+      ? "AppInspectorDemo"
+      : process.env.NEXT_PUBLIC_DB,
+  password:
+    process.env.NODE_ENV === "development"
+      ? "1234"
+      : process.env.NEXT_PUBLIC_PASS,
   port: 5432, // default PostgreSQL port
-  ssl: true,
+  ssl: process.env.NODE_ENV === "development" ? false : true,
 });
 
 client.connect();
