3c68e36664678f300bfb54efe404943adc40ffa0
Yassine <54895428+yassine955@users.noreply.github.com>
2025-03-04 17:49:58 +0100

    Update results.tsx

--- pages/app/results.tsx
+++ pages/app/results.tsx
@@ -103,7 +103,7 @@ export default function Home() {
                 <LoadingComp />
               </div>
             ) : (
-              <table className="max-sm:w-screen max-md:w-screen max-lg:w-5/6 max-xl:w-4/5 w-6/12 text-sm text-left bg-backgroundBlue ">
+              <table className="max-md:w-screen max-lg:w-5/6 max-xl:w-4/5 w-6/12 text-sm text-left bg-backgroundBlue">
                 <thead
                   className="text-xs text-cyan-600  sticky top-0 bg-white "
                   style={{
@@ -159,7 +159,10 @@ export default function Home() {
                     }
 
                     return (
-                      <tr className="font-medium bg-backgroundBlue border-b  text-darkBlueText">
+                      <tr
+                        key={row?.id}
+                        className="font-medium bg-backgroundBlue border-b text-darkBlueText"
+                      >
                         <th
                           scope="row"
                           className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
